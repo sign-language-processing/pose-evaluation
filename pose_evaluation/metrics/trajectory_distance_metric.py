688fceea61f666c7f7e3f9b5af85b161cf5a8499
Colin Leong <122366389+cleong110@users.noreply.github.com>
2025-01-30 15:53:51 -0500

    remove unused file

--- pose_evaluation/metrics/trajectory_distance_metric.py
+++
@@ -1,9 +0,0 @@
-from pose_evaluation.metrics.distance_metric import DistanceMetric
-
-
-class TrajectoryDistanceMetric(DistanceMetric):
-
-    """metrics that compare time-series trajectories"""
-    pass
-
-    
