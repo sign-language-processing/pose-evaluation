145b135a572000b3789ec117507cd5ddb02e15f8
Colin Leong <122366389+cleong110@users.noreply.github.com>
2025-03-18 16:21:39 -0400

    DTW Metric: new dtai metric, and remove double definition.

--- pose_evaluation/metrics/dtw_metric.py
+++ pose_evaluation/metrics/dtw_metric.py
@@ -1,5 +1,5 @@
-from fastdtw import fastdtw
-import numpy as np
+from fastdtw import fastdtw  # type: ignore
+from dtaidistance import dtw_ndim
 from scipy.spatial.distance import cdist
 import numpy.ma as ma  # pylint: disable=consider-using-from-import
 from tqdm import tqdm
@@ -26,18 +26,18 @@ class DTWAggregatedDistanceMeasure(AggregatedDistanceMeasure):
 
     def get_distance(self, hyp_data: ma.MaskedArray, ref_data: ma.MaskedArray, progress=False) -> float:
         keypoint_count = hyp_data.shape[2]  # Assuming shape: (frames, person, keypoints, xyz)
-        traj_distances = ma.empty(keypoint_count)  # Preallocate a NumPy array
+        trajectory_distances = ma.empty(keypoint_count)  # Preallocate a NumPy array
 
-        for i, (hyp_traj, ref_traj) in tqdm(
+        for i, (hyp_trajectory, ref_trajectory) in tqdm(
             enumerate(self._get_keypoint_trajectories(hyp_data, ref_data)),
             desc="getting dtw distances for trajectories",
             total=keypoint_count,
             disable=not progress,
         ):
-            distance, _ = fastdtw(hyp_traj, ref_traj, dist=self._calculate_pointwise_distances)
-            traj_distances[i] = distance  # Store distance in the preallocated array
-        traj_distances = ma.array(traj_distances)
-        return self._aggregate(traj_distances)
+            distance, _ = fastdtw(hyp_trajectory, ref_trajectory, dist=self._calculate_pointwise_distances)
+            trajectory_distances[i] = distance  # Store distance in the preallocated array
+        trajectory_distances = ma.array(trajectory_distances)
+        return self._aggregate(trajectory_distances)
 
     def _calculate_pointwise_distances(self, hyp_data: ma.MaskedArray, ref_data: ma.MaskedArray) -> ma.MaskedArray:
         raise NotImplementedError("_calculate_pointwise_distances must be a callable that can be passed to fastdtw")
@@ -86,27 +86,6 @@ class DTWAggregatedScipyDistanceMeasure(DTWAggregatedDistanceMeasure):
         return cdist(hyp_data, ref_data, metric=self.metric)  # type: ignore "no overloads match" but it works fine
 
 
-class DTWAggregatedPowerDistanceMeasure(DTWAggregatedDistanceMeasure):
-    def __init__(
-        self,
-        name="DTWAggregatedDistanceMeasure",
-        default_distance: float = 0,
-        aggregation_strategy: AggregationStrategy = "mean",
-        order=2,
-    ) -> None:
-        super().__init__(
-            name=name,
-            default_distance=default_distance,
-            aggregation_strategy=aggregation_strategy,
-        )
-        self.power = order
-
-    def _calculate_pointwise_distances(self, hyp_data: ma.MaskedArray, ref_data: ma.MaskedArray) -> ma.MaskedArray:
-        return masked_array_power_distance(
-            hyp_data=hyp_data, ref_data=ref_data, power=self.power, default_distance=self.default_distance
-        )
-
-
 class DTWOptimizedDistanceMeasure(DTWAggregatedDistanceMeasure):
     """Optimized according to https://github.com/slaypni/fastdtw/blob/master/fastdtw/_fastdtw.pyx#L71-L76
     This function runs fastest if the following conditions are satisfied:
@@ -115,7 +94,7 @@ class DTWOptimizedDistanceMeasure(DTWAggregatedDistanceMeasure):
             2) The dist input is a positive integer or None
     """
 
-    def __init__(
+    def __init__(  # pylint: disable=too-many-arguments,too-many-positional-arguments
         self,
         name="DTWOptimizedDistanceMeasure",
         default_distance: float = 0,
@@ -142,15 +121,56 @@ class DTWOptimizedDistanceMeasure(DTWAggregatedDistanceMeasure):
         hyp_data = hyp_data.filled(self.masked_fill_value).astype(float)
         ref_data = ref_data.filled(self.masked_fill_value).astype(float)
         keypoint_count = hyp_data.shape[2]  # Assuming shape: (frames, person, keypoints, xyz)
-        traj_distances = ma.empty(keypoint_count)  # Preallocate a NumPy array
+        trajectory_distances = ma.empty(keypoint_count)  # Preallocate a NumPy array
 
-        for i, (hyp_traj, ref_traj) in tqdm(
+        for i, (hyp_trajectory, ref_trajectory) in tqdm(
             enumerate(self._get_keypoint_trajectories(hyp_data, ref_data)),
             desc="getting dtw distances for trajectories",
             total=keypoint_count,
             disable=not progress,
         ):
-            distance, _ = fastdtw(hyp_traj, ref_traj, self.power)
-            traj_distances[i] = distance  # Store distance in the preallocated array
-        traj_distances = ma.array(traj_distances)
-        return self._aggregate(traj_distances)
+            distance, _ = fastdtw(hyp_trajectory, ref_trajectory, self.power)
+            trajectory_distances[i] = distance  # Store distance in the preallocated array
+        trajectory_distances = ma.array(trajectory_distances)
+        return self._aggregate(trajectory_distances)
+
+
+# https://forecastegy.com/posts/dynamic-time-warping-dtw-libraries-python-examples/
+class DTWDTAIImplementationDistanceMeasure(AggregatedDistanceMeasure):
+
+    def __init__(  # pylint: disable=too-many-arguments,too-many-positional-arguments
+        self,
+        name="dtaiDTWAggregatedDistanceMeasure",
+        default_distance: float = 0,
+        aggregation_strategy: AggregationStrategy = "mean",
+        masked_fill_value=0,
+        use_fast=True,
+    ) -> None:
+        super().__init__(
+            name=name,
+            default_distance=default_distance,
+            aggregation_strategy=aggregation_strategy,
+        )
+        self.masked_fill_value = masked_fill_value
+        self.use_fast = use_fast
+
+    def get_distance(self, hyp_data: ma.MaskedArray, ref_data: ma.MaskedArray, progress=False) -> float:
+        hyp_data = hyp_data.filled(self.masked_fill_value)
+        ref_data = ref_data.filled(self.masked_fill_value)
+        keypoint_count = hyp_data.shape[2]  # Assuming shape: (frames, person, keypoints, xyz)
+        trajectory_distances = ma.empty(keypoint_count)  # Preallocate a NumPy array
+
+        for i, (hyp_trajectory, ref_trajectory) in tqdm(
+            enumerate(self._get_keypoint_trajectories(hyp_data, ref_data)),
+            desc="getting dtw distances for trajectories",
+            total=keypoint_count,
+            disable=not progress,
+        ):
+            if self.use_fast:
+                distance = dtw_ndim.distance_fast(hyp_trajectory, ref_trajectory)  # about 8s per call
+            else:
+                distance = dtw_ndim.distance(hyp_trajectory, ref_trajectory)  # about 8s per call
+
+            trajectory_distances[i] = distance  # Store distance in the preallocated array
+        trajectory_distances = ma.array(trajectory_distances)
+        return self._aggregate(trajectory_distances)
