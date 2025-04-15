8249e6f20eaf4d4bd64790a3d6a43b182f5d5cef
Colin Leong <122366389+cleong110@users.noreply.github.com>
2025-04-15 21:47:32 +0000

    code to get translations of popsign ASL words

--- pose_evaluation/evaluation/dataset_parsing/dataset_utils.py
+++ pose_evaluation/evaluation/dataset_parsing/dataset_utils.py
@@ -115,3 +115,37 @@ def find_duplicates(df: pd.DataFrame, column: str):
     print(duplicate_rows)
 
     return duplicate_counts, duplicate_rows
+
+
+def convert_eng_to_ase_gloss_translations(df, asl_knowledge_graph_df, translations_only=False):
+    translation_df = asl_knowledge_graph_df[asl_knowledge_graph_df["relation"] == "has_translation"]
+    # translation_df = asl_knowledge_graph_df[asl_knowledge_graph_df["source"] == "asllex"]
+    translation_df.loc[:, "object"] = translation_df["object"].str.upper()
+    # translation_df["object"] = translation_df["object"].str.upper()
+
+    matching_translations = translation_df[translation_df["object"].isin(df["GLOSS"])]
+
+    selected_translations = []
+    for translated_word in matching_translations["object"].unique():
+        translations = matching_translations[matching_translations["object"] == translated_word]
+        translations_without_colon = []
+        for translation in translations["subject"].tolist():
+            translation = translation.split(":")[-1].upper()
+            translations_without_colon.append(translation)
+
+        if len(set(translations_without_colon)) == 1:
+            translated_word_without_lang = translated_word.split(":")[1]
+            translation = list(set(translations_without_colon))[0]
+
+            if translated_word_without_lang == translation:
+                selected_translations.append((translated_word, translation))
+
+    mapping_dict = dict(selected_translations)
+
+    if translations_only:
+        # Filter rows where GLOSS is in the mapping keys
+        df = df[df["GLOSS"].isin(mapping_dict)].copy()
+
+    # Apply the mapping safely using .loc
+    df.loc[:, "GLOSS"] = df["GLOSS"].map(mapping_dict).fillna(df["GLOSS"])
+    return df
