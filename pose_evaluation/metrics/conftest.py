06335b6c4a1c481dfd6e64ee18161d03c21e8319
Colin Leong <122366389+cleong110@users.noreply.github.com>
2025-04-16 19:55:07 +0000

    add conftest mixed-shape files

--- pose_evaluation/metrics/conftest.py
+++ pose_evaluation/metrics/conftest.py
@@ -67,3 +67,11 @@ def real_pose_files() -> List[Pose]:
 def real_refined_landmark_pose_file_paths() -> List[Path]:
     test_files_folder = Path("pose_evaluation") / "utils" / "test" / "test_data" / "mediapipe" / "refined_landmarks"
     return list(test_files_folder.glob("*.pose"))
+
+
+@pytest.fixture
+def real_mixed_shape_files() -> List[Pose]:
+    # pose_evaluation/utils/test/test_data/standard_landmarks
+    test_files_folder = Path("pose_evaluation") / "utils" / "test" / "test_data" / "mediapipe" / "mixed"
+    real_pose_files_list = [Pose.read(test_file.read_bytes()) for test_file in test_files_folder.glob("*.pose")]
+    return real_pose_files_list
