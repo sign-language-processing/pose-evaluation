057548d4c6160a965997121576a995baea74c6e3
Colin Leong <122366389+cleong110@users.noreply.github.com>
2025-04-17 19:50:37 +0000

    Save off in-gloss and out-gloss dfs for subsequent metric use to ensure each metric gets the same set of poses

--- pose_evaluation/evaluation/load_splits_and_run_metrics.py
+++ pose_evaluation/evaluation/load_splits_and_run_metrics.py
@@ -22,7 +22,8 @@ def combine_dataset_dfs(dataset_df_files: List[Path], splits: List[str], filter_
     for file_path in dataset_df_files:
         if file_path.exists():
             typer.echo(f"✅ Found: {file_path}")
-            df = pd.read_csv(file_path)
+            df = pd.read_csv(file_path, dtype={"GLOSS": str, "SPLIT": str, "VIDEO_ID": str, "POSE_FILE_PATH": str})
+
             df = df[df["SPLIT"].isin(splits)]
             df["DATASET"] = file_path.stem
             typer.echo(f"Loaded {len(df)} rows from splits: {splits}")
@@ -53,30 +54,69 @@ def run_metrics(
     gloss_count: Optional[int] = 5,
     out_gloss_multiplier=4,
     shuffle_metrics=True,
+    metric_count: Optional[int] = 10,
     additional_glosses: Optional[List[str]] = None,
+    shuffle_query_glosses=True,
 ):
 
-    gloss_vocabulary = df[STANDARDIZED_GLOSS_COL_NAME].unique().tolist()
+    query_gloss_vocabulary = df[STANDARDIZED_GLOSS_COL_NAME].unique().tolist()
 
     if gloss_count:
-        gloss_vocabulary = gloss_vocabulary[:gloss_count]
+        query_gloss_vocabulary = query_gloss_vocabulary[:gloss_count]
 
     if additional_glosses:
         # prepend them
-        print(f"Prepending additional glosses {additional_glosses}")
-        additional_glosses.extend(gloss_vocabulary)
-        gloss_vocabulary = additional_glosses
-        print(f"Gloss vocabulary is now length {len(gloss_vocabulary)}")
+        print(f"Prepending additional query glosses {additional_glosses}")
+        combined = additional_glosses + query_gloss_vocabulary
+        # Use dict.fromkeys() to remove duplicates while keeping order
+        query_gloss_vocabulary = list(dict.fromkeys(combined))
+        print(f"Query gloss vocabulary is now length {len(query_gloss_vocabulary)}")
+        print(f"Query gloss vocabulary: {query_gloss_vocabulary}")
+
+    if shuffle_query_glosses:
+        random.shuffle(query_gloss_vocabulary)
 
     if shuffle_metrics:
         random.shuffle(metrics)
 
-    for g_index, gloss in enumerate(gloss_vocabulary):
-        in_gloss_df = df[df[STANDARDIZED_GLOSS_COL_NAME] == gloss]
-        out_gloss_df = df[df[STANDARDIZED_GLOSS_COL_NAME] != gloss]
+    if metric_count is not None:
+        typer.echo(f"Selecting {metric_count} of {len(metrics)} metrics")
+        metrics = metrics[:metric_count]
 
-        other_class_count = len(in_gloss_df) * out_gloss_multiplier
-        out_gloss_df = out_gloss_df.sample(n=other_class_count, random_state=42)
+    gloss_dfs_folder = out_path / "gloss_dfs"
+    gloss_dfs_folder.mkdir(exist_ok=True, parents=True)
+
+    scores_path = out_path / "scores"
+    scores_path.mkdir(exist_ok=True, parents=True)
+
+    for g_index, gloss in enumerate(
+        tqdm(
+            query_gloss_vocabulary,
+            total=len(query_gloss_vocabulary),
+            desc=f"Running evaluations for all {len(query_gloss_vocabulary)} glosses",
+        )
+    ):
+
+        in_gloss_df_path = gloss_dfs_folder / f"{gloss}_in.csv"
+        if in_gloss_df_path.is_file():
+            print(f"Reading in-gloss df from {in_gloss_df_path}")
+            in_gloss_df = pd.read_csv(in_gloss_df_path, index_col=0, dtype={STANDARDIZED_GLOSS_COL_NAME: str})
+        else:
+            print(f"Writing out-gloss df to {in_gloss_df_path}")
+            in_gloss_df = df[df[STANDARDIZED_GLOSS_COL_NAME] == gloss]
+            in_gloss_df.to_csv(in_gloss_df_path)
+
+        out_gloss_df_path = gloss_dfs_folder / f"{gloss}_out.csv"
+        if out_gloss_df_path.is_file():
+            print(f"Reading in out-gloss-df from {out_gloss_df_path}")
+            out_gloss_df = pd.read_csv(out_gloss_df_path, index_col=0, dtype={STANDARDIZED_GLOSS_COL_NAME: str})
+        else:
+            print(f"Writing out-gloss df to {out_gloss_df_path}")
+            out_gloss_df = df[df[STANDARDIZED_GLOSS_COL_NAME] != gloss]
+            other_class_count = len(in_gloss_df) * out_gloss_multiplier
+            out_gloss_df = out_gloss_df.sample(n=other_class_count, random_state=42)
+            out_gloss_df.to_csv(out_gloss_df_path)
+
         ref_df = pd.concat([in_gloss_df, out_gloss_df], ignore_index=True)
 
         pose_data = load_pose_files(in_gloss_df)
@@ -88,17 +128,22 @@ def run_metrics(
 
         for i, metric in enumerate(metrics):
             typer.echo("*" * 60)
-            typer.echo(f"Gloss #{g_index}/{len(gloss_vocabulary)}: {gloss}")
-            typer.echo(f"Testing with {len(in_gloss_df)} in-gloss files, and {len(out_gloss_df)} out-gloss")
             typer.echo(f"Metric #{i}/{len(metrics)}: {metric.name}")
             typer.echo(f"Metric #{i}/{len(metrics)} Signature: {metric.get_signature().format()}")
+            typer.echo(f"Gloss #{g_index}/{len(query_gloss_vocabulary)}: {gloss}")
+            typer.echo(f"Testing with {len(in_gloss_df)} in-gloss files, and {len(out_gloss_df)} out-gloss")
+
             results = defaultdict(list)
-            results_path = out_path / f"{gloss}_{metric.name}_outgloss_{out_gloss_multiplier}x_score_results.csv"
+            results_path = scores_path / f"{gloss}_{metric.name}_outgloss_{out_gloss_multiplier}x_score_results.csv"
             if results_path.exists():
                 print(f"Results for {results_path} already exist. Skipping!")
                 continue
 
-            for _, hyp_row in tqdm(in_gloss_df.iterrows()):
+            for _, hyp_row in tqdm(
+                in_gloss_df.iterrows(),
+                total=len(in_gloss_df),
+                desc=f"Calculating all {len(in_gloss_df)*len(ref_df)} distances for gloss #{g_index}/{len(query_gloss_vocabulary)}({gloss}) against all {len(ref_df)} references",
+            ):
                 hyp_path = hyp_row["POSE_FILE_PATH"]
                 hyp_pose = pose_data[hyp_path].copy()
 
@@ -154,12 +199,13 @@ def main(
     splits: Optional[str] = typer.Option(
         None, help="Comma-separated list of splits to process (e.g., 'train,val,test'), default is 'test' only"
     ),
-    gloss_count: Optional[int] = typer.Option(5, help="Number of glosses to select"),
+    gloss_count: Optional[int] = typer.Option(12, help="Number of glosses to select"),
     additional_glosses: Optional[str] = typer.Option(
         None,
         help="Comma-separated list of additional glosses to use for testing in addition to the ones selected by gloss_count",
     ),
     out_gloss_multiplier: Optional[int] = typer.Option(4, help="Number of out-of-gloss items to sample"),
+    metric_count: Optional[int] = typer.Option(None, help="Number of metrics to sample"),
     filter_en_vocab: Annotated[
         bool,
         typer.Option(
@@ -200,10 +246,13 @@ def main(
         metrics=metrics,
         gloss_count=gloss_count,
         out_gloss_multiplier=out_gloss_multiplier,
+        shuffle_metrics=True,
+        metric_count=metric_count,
         additional_glosses=additional_glosses,
     )
 
 
 if __name__ == "__main__":
     app()
-# python pose_evaluation/evaluation/load_splits_and_run_metrics.py dataset_dfs/*.csv --additional-glosses "RUSSIA,BRAG,HOUSE,HOME,WORM,REFRIGERATOR,BLACK,SUMMER" 2>&1|tee out2.txt
+# conda activate /opt/home/cleong/envs/pose_eval_src && cd /opt/home/cleong/projects/pose-evaluation && python pose_evaluation/evaluation/load_splits_and_run_metrics.py dataset_dfs/*.csv --additional-glosses "RUSSIA,BRAG,HOUSE,HOME,WORM,REFRIGERATOR,BLACK,SUMMER,SICK,REALSICK,WEATHER,MEETING,COLD,WINTER,THANKSGIVING,THANKYOU,HUNGRY,FULL" 2>&1|tee out/$(date +%s).txt
+# stat -c "%y" metric_results/scores/* | cut -d':' -f1 | sort | uniq -c # get the timestamps/hour
