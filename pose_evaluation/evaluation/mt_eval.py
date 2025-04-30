b40d6bb3051dd70cc31a27cf4fbd1cec7d0cc0f7
Colin Leong <122366389+cleong110@users.noreply.github.com>
2025-04-30 13:34:36 +0000

    Update mt-eval to use raw and imputed poses with more keypoints

--- pose_evaluation/evaluation/mt_eval.py
+++ pose_evaluation/evaluation/mt_eval.py
@@ -21,15 +21,36 @@ def chunks(lst, n):
 
 if __name__ == "__main__":
     # point this to the folders with 0.pose, 1.pose, etc.
-    hyp_folder = Path("/opt/home/cleong/data/zifan_signsuisse/system_outputs/signsuisse_test/sockeye/")
+    # Zifan says:
+    # for reference, always use the raw pose for full keypoints
+    # for sign_mt/sign_mt_v2, same
+    # for sockeye, the MT system does not produce the face, so we have <id>.pose (direct system output) and <id>.imputed.pose (the face imputed with an average face). I'd use the imputed one for completeness
+    sockeye_folder = Path("/opt/home/cleong/data/zifan_signsuisse/system_outputs/signsuisse_test/sockeye/")
+    sign_mt_v2_folder = Path("/opt/home/cleong/data_munging/local_data/zifan_signsuisse/signsuisse_test/sign_mt_v2")
     ref_folder = Path("/opt/home/cleong/data/zifan_signsuisse/system_outputs/signsuisse_test/ref/")
+
+    item_count = 1000
+
+    # use raw for reference
+    ref_paths = [ref_folder / f"{i}.raw.pose" for i in range(item_count)]
+
+    # use raw for sign_mt_v2
+    sign_mt_v2_paths = [sign_mt_v2_folder / f"{i}.raw.pose" for i in range(item_count)]
+
+    # use imputed for sockeye
+    sockeye_paths = [sockeye_folder / f"{i}.imputed.pose" for i in range(item_count)]
+
+    system_paths = {
+        "sockeye": sockeye_paths,
+        "sign_mt_v2": sign_mt_v2_paths,
+    }
 
     system = "pose-eval"
     entries = []
     metrics = get_metrics()
 
     # out of 10k+ metric combinations, filter to just the top 10
-    FILTER_METRICS = true
+    FILTER_METRICS = True
     if FILTER_METRICS:
         metrics = get_filtered_metrics(
             metrics,
@@ -46,35 +67,40 @@ if __name__ == "__main__":
     for batch_id, metric_chunk in enumerate(chunks(metrics, chunk_size)):
         chunk_start = batch_id * chunk_size
         chunk_end = min(chunk_start + chunk_size, len(metrics))
-        output_csv_path = f"/opt/home/cleong/data/zifan_signsuisse/metrics/metrics.{system}-{len(metrics)}metrics-metric{chunk_start}-metric{chunk_end}.csv"
 
-        print(f"Running metrics {chunk_start} to {chunk_end} and saving to {output_csv_path}")
-        entries = []
+        for system, hyp_paths in system_paths.items():
 
-        for i in tqdm(range(1000), desc=f"Running metrics {chunk_start}-{chunk_end} on pose pairs"):
-            hyp_file = hyp_folder / f"{i}.pose"
-            ref_file = ref_folder / f"{i}.pose"
-            hyp = Pose.read(Path(hyp_file).read_bytes())
-            ref = Pose.read(Path(ref_file).read_bytes())
+            output_csv_path = f"/opt/home/cleong/data/zifan_signsuisse/metrics/metrics.{system}-{len(metrics)}metrics-metric{chunk_start}-metric{chunk_end}.csv"
 
-            entry = {
-                "data": "signsuisse_test",
-                "system": system,
-                "example_id": i,
-                # these two can be copied from existing metrics.{system}.csv files
-                # 'source': language_map[language]['source'],
-                # 'target': language_map[language]['target'],
-            }
+            print(f"Running metrics for {system}, {chunk_start} to {chunk_end} and saving to {output_csv_path}")
+            entries = []
 
-            for metric in tqdm(metric_chunk, disable=len(metric_chunk) < 20, desc="Running Metrics"):
-                try:
-                    score = metric.score_with_signature(hyp, ref)
-                    entry[f"{metric.name}"] = score.score
-                except KeyError as e:
-                    pass
-                except ValueError as e:
-                    print(f"Error on {metric.name}: {e}")
+            for i in tqdm(
+                range(item_count), desc=f"Running metrics {chunk_start}-{chunk_end} on pose pairs for system {system}"
+            ):
+                hyp_file = hyp_paths[i]
+                ref_file = ref_paths[i]
+                hyp = Pose.read(Path(hyp_file).read_bytes())
+                ref = Pose.read(Path(ref_file).read_bytes())
 
-            entries.append(entry)
+                entry = {
+                    "data": "signsuisse_test",
+                    "system": system,
+                    "example_id": i,
+                    # these two can be copied from existing metrics.{system}.csv files
+                    # 'source': language_map[language]['source'],
+                    # 'target': language_map[language]['target'],
+                }
 
-        save_results(entries, output_csv_path)
+                for metric in tqdm(metric_chunk, disable=len(metric_chunk) < 20, desc="Running Metrics"):
+                    try:
+                        score = metric.score_with_signature(hyp, ref)
+                        entry[f"{metric.name}"] = score.score
+                    except KeyError as e:
+                        pass
+                    except ValueError as e:
+                        print(f"Error on {metric.name}: {e}")
+
+                entries.append(entry)
+
+            save_results(entries, output_csv_path)
